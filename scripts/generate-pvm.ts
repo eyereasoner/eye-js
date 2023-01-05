204ab965ff16735989b6f7396cc91fd5e0bb7b50
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-01-06 02:00:38 +1100

    fix: npm install working again

--- scripts/generate-pvm.ts
+++ scripts/generate-pvm.ts
@@ -1,10 +1,17 @@
 import fs from 'fs';
 import path from 'path';
-import { queryOnce, SWIPL } from '..';
-import EYE from '../eye/eye.pl';
+import { SWIPLModule } from 'swipl-wasm/dist/common';
+import { queryOnce } from '../lib/query';
+// @ts-ignore
+import SWIPL from '../lib/swipl-bundled.temp';
 
 (async () => {
-  const Module = await SWIPL({ arguments: ['-q', '-f', 'eye.pl'], preRun: (module) => { module.FS.writeFile('eye.pl', EYE); } });
+  const Module = await SWIPL({
+    arguments: ['-q', '-f', 'eye.pl'],
+    preRun: (module: SWIPLModule) => {
+      module.FS.writeFile('eye.pl', fs.readFileSync(path.join(__dirname, '..', 'eye', 'eye.pl')).toString());
+    },
+  });
   queryOnce(Module, 'main', ['--image', 'eye.pvm']);
   fs.writeFileSync(path.join(__dirname, '..', 'eye', 'eye.pvm'), Module.FS.readFile('eye.pvm'));
 })();
