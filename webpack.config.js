696bcb1faed7a73e8106218d781ae71729d0612e
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-01-15 14:56:04 +1100

    chore: version by folders

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 
-const version = process.argv.find(name => name.startsWith('--name='))?.slice(7);
+const version = process.argv.find(name => name.startsWith('--name='))?.slice(8);
 
 module.exports = {
   mode: 'production',
   entry: './dist/index.js',
   output: {
-    filename: version ? version + '.js' : 'bundle.js',
-    path: path.resolve(__dirname, 'bundle'),
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'bundle', ...(version ? version.split('.') : [])),
   },
   resolve: {
     fallback: {
