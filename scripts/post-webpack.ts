e9ca7405438e67db48ba71321a5872d7d059430d
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-01-25 14:01:17 +1100

    feat: produce dynamic imports

--- scripts/post-webpack.ts
+++ scripts/post-webpack.ts
@@ -1,16 +1,25 @@
-import fs from 'fs';
+/* eslint-disable import/no-extraneous-dependencies */
+import fs from 'fs-extra';
 import path from 'path';
 
 let version = process.argv.find((name) => name.startsWith('--name='))?.slice(8).split('.');
 
 if (version) {
   version = ['bundle', ...version];
+
+  fs.writeFileSync(
+    path.join(__dirname, '..', ...version, 'dynamic-import.js'),
+    fs.readFileSync(
+      path.join(__dirname, '..', ...version, 'index.js'),
+    ).toString().replace('var eyereasoner;', 'export var eyereasoner;'),
+  );
+
   for (let i = 1; i < version.length; i += 1) {
     const destDir = path.join(__dirname, '..', ...version.slice(0, i), 'latest');
 
     if (!fs.existsSync(destDir)) {
       fs.mkdirSync(destDir, { recursive: true });
     }
-    fs.copyFileSync(path.join(__dirname, '..', ...version, 'index.js'), path.join(destDir, 'index.js'));
+    fs.copySync(path.join(__dirname, '..', ...version), destDir);
   }
 }
