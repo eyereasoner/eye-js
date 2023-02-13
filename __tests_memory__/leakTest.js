08b48aebd0490e257505fe0c2458e6c7ced5e2a6
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-02-13 11:18:37 +1100

    chore: reduce memory test flakiness

--- __tests_memory__/leakTest.js
+++ __tests_memory__/leakTest.js
@@ -23,8 +23,14 @@ function toMb(value) {
 async function main() {
   const initial = process.memoryUsage().heapUsed;
   let maxStart = 0;
+  
+  // Warmup
+  for (let i = 0; i < 100; i++) {
+    // Allow for GC
+    await new Promise(res => setTimeout(res, 0));
 
-  console.log(`Starting with usage of: ${toMb(initial)}MB`)
+    await n3reasoner(data, query)
+  }
 
   for (let i = 0; i < 200; i++) {
     // Allow for GC
@@ -35,7 +41,7 @@ async function main() {
   }
 
   if ((maxStart - initial) > 50 * (1024 ** 2)) {
-    throw new Error(`Exceeded initial memory consumption of ${toMb(initial)}MB by more than 50MB, reaching ${toMb(maxStart)}MB in the first 200 iterations`)
+    throw new Error(`Exceeded initial memory consumption of ${toMb(initial)}MB by more than 50MB, reaching ${toMb(maxStart)}MB in the first 100 iterations`)
   }
 
   console.log(`Maximum memory usage in first 200 iterations: ${toMb(maxStart)}MB`)
@@ -52,17 +58,31 @@ async function main() {
 
     if (i !== 0 && (i % 50 === 0)) {
       console.log(`Max usage after ${i} iterations: ${toMb(maxCont)}MB`);
-
-      if ((maxCont - maxStart) > 5 * (1024 ** 2)) {
-        throw new Error(`${toMb(maxCont - maxStart)}MB increase encountered after ${i} iterations, max allowed: 5MB`)
-      }
     }
 
-    await n3reasoner(data, query)
+    await n3reasoner(data, query);
+  }
+
+  let minTail = maxCont;
+
+  // To avoid test flakiness from general variability in tests
+  // compare the highest value on the first 50 tests to the lowest value
+  // in the last 50 tests
+  for (let i = 0; i <= 50; i++) {
+    await new Promise(res => setTimeout(res, 0));
+
+    minTail = Math.min(process.memoryUsage().heapUsed, minTail);
+    await n3reasoner(data, query);
+  }
+
+  console.log(`The minimum value found in the tail was ${toMb(minTail)}MB`)
+
+  if ((minTail - maxStart) > 1 * (1024 ** 2)) {
+    throw new Error(`${toMb(maxCont - maxStart)}MB increase encountered after ${i} iterations, max allowed: 1MB`)
   }
 
   if ((maxCont - initial) > 50 * (1024 ** 2)) {
-    throw new Error(`Exceeded initial memory consumption of ${toMb(initial)}MB by more than 50MB, reaching ${toMb(maxCont)}MB`)
+    throw new Error(`Exceeded initial memory consumption of ${toMb(initial)}MB by more than 50MB, reaching ${toMb(minTail)}MB`)
   }
 }
 
