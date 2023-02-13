0f5d720cd7847ba22f6b354c08e0890ac1e8e768
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-02-13 11:50:09 +1100

    chore: add browser tests with playwright

---
+++ __tests__/e2e-test.ts
@@ -0,0 +1,52 @@
+import type { Server } from 'http';
+import { firefox, chromium, type BrowserType } from 'playwright';
+import { createTestApp } from '../__test_utils__/serve';
+import { data } from '../data/socrates';
+
+describe('Testing browsers', () => {
+  let server: Server;
+
+  // Create the server
+  beforeAll(() => {
+    const app = createTestApp();
+    server = app.listen(3001);
+  });
+
+  afterAll(async () => {
+    await new Promise((res, rej) => {
+      server.on('close', res);
+      server.on('error', rej);
+      server.close(res);
+    });
+  });
+
+  ([[firefox, 'firefox'], [chromium, 'chromium']] as [BrowserType<{}>, string][]).forEach(
+    ([browserType, browserName]) => {
+      it(`should be able to call the execute function in ${browserName}`, async () => {
+        const browser = await browserType.launch();
+        const page = await browser.newPage();
+
+        await page.goto('http://localhost:3001/');
+        await expect(page.textContent('textarea[id=data]').then((r) => r?.trim())).resolves.toEqual(data.trim());
+        await expect(page.textContent('div[id=result]').then((r) => r?.trim())).resolves.toEqual('');
+
+        await page.click('button[id=execute]');
+        // Time for new result to be inserted in the DOM
+        await new Promise((res) => { setTimeout(res, 1000); });
+
+        await expect(page.textContent('textarea[id=data]').then((r) => r?.trim())).resolves.toEqual(data.trim());
+        await expect(page.textContent('div[id=result]').then((r) => r?.trim())).resolves.toEqual('@prefix rdfs: .@prefix : .:Socrates a :Mortal.');
+
+        await page.click('button[id=clear]');
+        // Time for new result to be inserted in the DOM
+        await new Promise((res) => { setTimeout(res, 1000); });
+
+        await expect(page.textContent('textarea[id=data]').then((r) => r?.trim())).resolves.toEqual(data.trim());
+        await expect(page.textContent('div[id=result]').then((r) => r?.trim())).resolves.toEqual('');
+
+        await page.close();
+        await browser.close();
+      });
+    },
+  );
+});
--- __tests_memory__/browserLeakTest.js
+++ __tests_memory__/browserLeakTest.js
@@ -3,29 +3,10 @@ const fs = require('fs');
 const path = require('path');
 const {run} = require('@memlab/api');
 const rateLimit = require('express-rate-limit');
-
-const app = express();
-
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000,
-	max: 1_000,
-	standardHeaders: true,
-	legacyHeaders: false,
-});
-
-app.use(limiter);
-
-app.get('/', (_, res) => {
-  res.setHeader('content-type', 'text/html');
-  fs.createReadStream(path.join(__dirname, 'browser.html')).pipe(res);
-});
-
-app.get('/index.js', (_, res) => {
-  res.setHeader('content-type', 'text/javascript');
-  fs.createReadStream(path.join(__dirname, '..', 'bundle', 'index.js')).pipe(res);
-});
+const { createTestApp } = require('../__test_utils__/serve')
 
 async function main() {
+  const app = createTestApp();
   const server = app.listen(3000);
 
   const leaks = await run({scenario: {
