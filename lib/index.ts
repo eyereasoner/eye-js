b026323c0386408636d460d44bca8e53a96b850e
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-04-30 12:21:27 +0200

    feat: remove custom bundle

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,9 @@
-/// <reference types="emscripten" />
-
-import type { SWIPLModule } from 'swipl-wasm/dist/common';
-// @ts-ignore
-import SWIPL_BUNDLE from './swipl-bundled.temp';
-
 export * from './query';
 export * from './transformers';
-// eslint-disable-next-line no-unused-vars
-export const SWIPL: (options?: Partial<EmscriptenModule>) => Promise<SWIPLModule> = SWIPL_BUNDLE;
-
+export {
+  /**
+   * @deprecated Import from 'swipl-wasm' instead.
+   */
+  default as SWIPL,
+} from './swipl-bundled.temp';
 export { default as EYE_PVM } from './eye';
