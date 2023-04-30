d5c45563eadde3070da5f655e42c749b6235c084
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-04-30 23:59:50 +0200

    chore: add deep taxonomy benchmark to performance tests

--- perf/socrates.ts
+++ perf/socrates.ts
@@ -1,11 +1,15 @@
 import { data, query } from '../data/socrates';
-import { queryOnce, SwiplEye } from '../dist';
+import { queryOnce, SwiplEye, n3reasoner } from '../dist';
+import { generateDeepTaxonomy } from 'deep-taxonomy-benchmark';
+import { Parser } from 'n3';
 
 // The results recorded using DELL XPS 15 9520 (32GB RAM)
 // Initialise SWIPL        : 110.441ms (averages about 50-60ms on subsequent loads in the same session)
 // Load data.n3            : 0.081ms
 // Load query.n3           : 0.03ms
 // Execute query           : 10.882ms
+// Run socrates query 100 times with n3reasoner    : 4.596s
+// Run deep taxonomy benchmark                     : 5.486s
 async function main() {
   console.log('Testing performance of socrates query using eye.pvm')
 
@@ -26,6 +30,23 @@ async function main() {
   console.time('Execute query\t\t');
   queryOnce(Module, 'main', ['--nope', '--quiet', './data.n3', '--query', './query.n3']);
   console.timeEnd('Execute query\t\t');
+
+  console.time('Run socrates query 100 times with n3reasoner\t');
+  for (let i = 0; i < 100; i++) {
+    await n3reasoner(data, query)
+  }
+  console.timeEnd('Run socrates query 100 times with n3reasoner\t');
+
+  const dtb = [
+    ...generateDeepTaxonomy(45, true),
+    ...(new Parser({ format: 'n3' })).parse('{ ?s a ?o . ?o <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?o2 . } => { ?s a ?o2 . } .'),
+  ]
+
+  console.time('Run deep taxonomy benchmark\t\t\t');
+  for (let i = 0; i < 5; i++) {
+    await n3reasoner(dtb)
+  }
+  console.timeEnd('Run deep taxonomy benchmark\t\t\t');
 
   console.log()
 }
