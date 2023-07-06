952d9fc3c081a62c161e22dc717958042a9335a5
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-07-07 09:45:54 +1000

    fix: update to eye v4.4.6

--- data/socrates.ts
+++ data/socrates.ts
@@ -25,8 +25,7 @@ export const result = `
 @prefix : <http://example.org/socrates#>.
 
 :Socrates a :Human.
-:Socrates a :Mortal.
-`;
+:Socrates a :Mortal.`;
 
 export const dataStar = `
 @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>.
