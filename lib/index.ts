0630d811e2428457f46c6ab62d37cfb93409d96e
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-10-20 13:47:02 +0100

    BREAKING CHANGE: remove SWIPL export

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,4 @@
 /* istanbul ignore file */
 export * from './query';
 export * from './transformers';
-export {
-  /**
-   * @deprecated Import from 'swipl-wasm' instead.
-   */
-  default as SWIPL,
-} from 'swipl-wasm/dist/swipl/swipl-bundle-no-data';
 export { default as EYE_PVM } from './eye';
