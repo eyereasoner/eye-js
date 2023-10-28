6cff610278dedd5352d1e106e6dfc9409fc1c767
Jos De Roo <josderoo@gmail.com>
2023-10-28 12:28:15 +0200

    BREAKING CHANGE: graph term scope of blank nodes and Skolem IRIs for graffiti

--- data/blogic.ts
+++ data/blogic.ts
@@ -1,6 +1,7 @@
 export const data = `
 @prefix : <urn:example:> .
 @prefix log: <http://www.w3.org/2000/10/swap/log#> .
+@prefix skolem: <http://eyereasoner.github.io/.well-known/genid/8b98b360-9a70-4845-b52c-c675af60ad01#> .
 
 :Alice a :Person .
 
