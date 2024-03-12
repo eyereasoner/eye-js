1290921eaeeccc52e7d3179f245c5aaf11f9ac20
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-03-12 21:00:17 +0000

    chore: fix pvm generation

--- scripts/generate-pvm.ts
+++ scripts/generate-pvm.ts
@@ -9,7 +9,7 @@ async function main() {
   const EYE_URL = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json')).toString()).config[nm].url;
   const releaseInfo = (await (await fetchRetry(EYE_URL)).json());
   return generateImageFile(
-    `https://raw.githubusercontent.com/eyereasoner/${nm}/${releaseInfo.tag_name}/${nm}.pl`,
+    `https://raw.githubusercontent.com/eyereasoner/${nm}/${releaseInfo.tag_name}/${nm.split('-')[0]}.pl`,
     path.join(__dirname, '..', 'lib', `${nm}.ts`),
   );
 }
