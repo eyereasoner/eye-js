242fd14347267fe11c31e29f24a24617241a129b
Michael Nelson <absoludity@gmail.com>
2025-04-03 08:26:55 +1100

    Refactor: ensure posix path for emscripten FS.

--- lib/bin/main.ts
+++ lib/bin/main.ts
@@ -17,13 +17,18 @@ export async function mainFunc(proc: NodeJS.Process) {
   for (const arg of proc.argv.slice(2)) {
     const p = path.join(proc.cwd(), arg);
     if (fs.existsSync(p)) {
+      // For the Emscripten FS, we need to ensure posix separators
+      // for any relative sub-path that may have been typed in
+      // on w32 as subdir\socrates.n3
+      const posixPath = arg.replace(path.sep, path.posix.sep);
+
       // Create any subdirectories needed for this file path
-      const dirname = path.dirname(arg);
+      const dirname = path.dirname(posixPath);
       // @ts-ignore: mkdirTree exists in Emscripten FS but is not typed.
       Module.FS.mkdirTree(dirname);
 
       // Now write the file to the correct path
-      Module.FS.writeFile(arg, fs.readFileSync(p));
+      Module.FS.writeFile(posixPath, fs.readFileSync(p));
     }
   }
 
